dfda3d3ac673e46b59927480f1df1c3656a8e2d8
AyushChandekar <ayush.pc19@gmail.com>
2026-02-09 00:59:53 +0530

    input field commands

--- learnn/lib/views/pages/profilePage.dart
+++ learnn/lib/views/pages/profilePage.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
-
-class profilePage extends StatelessWidget {
+class profilePage extends StatefulWidget {
   const profilePage({super.key});
 
   @override
+  State<profilePage> createState() => _profilePageState();
+}
+
+class _profilePageState extends State<profilePage> {
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is Profile Page"),);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          TextField(controller: controller,decoration: InputDecoration(border: OutlineInputBorder()),
+          onEditingComplete: () {
+            setState(() {
+              
+            });
+          },),
+          Text(controller.text),
+        ],
+      ),
+    );
   }
 }
