759ff0bf92dbec8c2484ab1cf3f07f92d1ba3e8f
AyushChandekar <ayush.pc19@gmail.com>
2026-02-28 20:37:47 +0530

    adding the settings thing

---
+++ learnn/lib/views/pages/settingsPage.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool? isCheckedw = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+  String? menuItem;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              DropdownButton(
+                value: menuItem,
+                hint: Text("clikie"),
+                items: [
+                  DropdownMenuItem(child: Text("other1"), value: "other1"),
+                  DropdownMenuItem(child: Text("other2"), value: "other2"),
+                  DropdownMenuItem(child: Text("other3"), value: "other3"),
+                ],
+                onChanged: (String? value) {
+                  setState(() {
+                    menuItem = value;
+                  });
+                },
+              ),
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+                onEditingComplete: () => setState(() {}),
+              ),
+              Text(controller.text),
+              Checkbox(
+                tristate: true,
+                value: isCheckedw,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isCheckedw = value;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                tristate: true,
+                title: Text("Click me 🥹"),
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              Switch.adaptive(
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              SwitchListTile.adaptive(
+                title: Text("this is switch"),
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                  print(isSwitched.toString());
+                },
+              ),
+              Slider.adaptive(
+                min: 0,
+                max: 100,
+                divisions: 10,
+                value: sliderValue,
+                onChanged: (double value) {
+                  setState(() {
+                    sliderValue = value;
+                  });
+                  print(sliderValue);
+                },
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    splashColor: const Color.fromARGB(255, 54, 212, 244),
+                    onTap: () {
+                      print('faultie boxie😗clicked bruh');
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 100,
+
+                      decoration: BoxDecoration(
+                        // Replace color with decoration
+                        color: const Color.fromARGB(31, 128, 105, 149),
+                        borderRadius: BorderRadius.circular(12), // Add this
+                      ),
+                    ),
+                  ),
+                  //issued one ~ ^
+                  Ink(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(18, 128, 105, 149),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: InkWell(
+                      splashColor: const Color.fromARGB(255, 111, 37, 222),
+                      borderRadius: BorderRadius.circular(12),
+                      onTap: () {
+                        print('boxie😘clicked bruh');
+                      },
+                      child: Container(height: 100, width: 100),
+                    ),
+                  ),
+                ],
+              ),
+
+              // fixed one ^
+              GestureDetector(
+                onTap: () {
+                  print('giffie clicked bruh');
+                },
+                child: Image.asset('assets/images/ezgif.com-effects.gif'),
+              ),
+
+              Image.asset('assets/images/pxfuel.jpg'),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(174, 3, 137, 57),
+                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                  shadowColor: const Color.fromARGB(255, 246, 255, 0),
+                ),
+                child: Text("submit"),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("submit🧟‍♀️")),
+              FilledButton(onPressed: () {}, child: Text("submit🤑")),
+              TextButton(onPressed: () {}, child: Text("submit🕹️")),
+              OutlinedButton(onPressed: () {}, child: Text("submit👆")),
+              CloseButton(),
+              IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              BackButton(color: Colors.blue),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- learnn/lib/views/widgetTree.dart
+++ learnn/lib/views/widgetTree.dart
@@ -3,9 +3,10 @@ import 'package:learnn/data/notifiers.dart';
 import 'package:learnn/views/pages/homePage.dart';
 import 'package:learnn/views/pages/locationPage.dart';
 import 'package:learnn/views/pages/profilePage.dart';
+import 'package:learnn/views/pages/settingsPage.dart';
 import 'package:learnn/views/widgets/navbarWidget.dart';
 
-List<Widget> pages = [homePage(), locationPage(), profilePage()];
+List<Widget> pages = [homePage(), locationPage(), ProfilePage()];
 
 class widgetTree extends StatelessWidget {
   const widgetTree({super.key});
@@ -18,15 +19,27 @@ class widgetTree extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              isDarkModeNotifier.value = !isDarkModeNotifier.value; 
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
             },
             icon: ValueListenableBuilder(
               valueListenable: isDarkModeNotifier,
               builder: (context, isDarkMode, child) {
-                return Icon(
-                  isDarkMode? Icons.light_mode: Icons.dark_mode,);
+                return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode);
               },
             ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage();
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
           ),
         ],
       ),
