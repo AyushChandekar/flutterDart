99dd361ac885664f8926c07c3dee73d606d39e4f
AyushChandekar <ayush.pc19@gmail.com>
2026-03-04 23:55:44 +0530

    welcome page added

---
+++ learnn/lib/main9.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:learnn/data/notifiers.dart';
+import 'package:learnn/views/pages/welcomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.purple,
+              brightness: isDarkMode ? Brightness.dark : Brightness.light,
+            ),
+          ),
+          home: WelcomePage(),
+        );
+      },
+    );
+  }
+}
--- learnn/lib/views/widgets/navbarWidget.dart
+++ learnn/lib/views/widgets/navbarWidget.dart
@@ -22,5 +22,5 @@ class navbarWidget extends StatelessWidget {
         );
       },
     );
-  }
+  } 
 }
