83c88d143c6127604795519030f1e0424e00316e
AyushChandekar <ayush.pc19@gmail.com>
2026-03-06 23:45:07 +0530

    bit of bug fixing

--- learnn/lib/views/pages/loginPage.dart
+++ learnn/lib/views/pages/loginPage.dart
@@ -3,8 +3,9 @@ import 'package:learnn/views/widgetTree.dart';
 import 'package:learnn/views/widgets/heroWidget.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
 
+  final String title;
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -33,45 +34,49 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            heroWidget(title: "Login Here"),
-            SizedBox(height: 20),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hint: Text("Enter Email"),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              heroWidget(title: widget.title),
+              SizedBox(height: 20),
+              TextField(
+                controller: controllerEmail,
+                decoration: InputDecoration(
+                  hint: Text("Enter Email"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: controllerPassword,
+                decoration: InputDecoration(
+                  hint: Text("Enter Password"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: () {
+                  onLoginPressed();
+                },
+                child: Text(widget.title),
+                style: FilledButton.styleFrom(
+                  minimumSize: Size(double.infinity, 45.0),
+                  backgroundColor: const Color.fromARGB(255, 55, 136, 228),
                 ),
               ),
-              onEditingComplete: () => setState(() {}),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hint: Text("Enter Password"),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onEditingComplete: () => setState(() {}),
-            ),
-            SizedBox(height: 20),
-            FilledButton(
-              onPressed: () {
-                onLoginPressed();
-              },
-              child: Text('Get Started'),
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 45.0),
-                backgroundColor: const Color.fromARGB(255, 55, 136, 228),
-              ),
-            ),
-          ],
+              SizedBox(height: 50.0),
+            ],
+          ),
         ),
       ),
     );
--- learnn/lib/views/pages/welcomePage.dart
+++ learnn/lib/views/pages/welcomePage.dart
@@ -9,59 +9,63 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset('assets/lotties/EarthRotating.json', height: 200),
-            FittedBox(child: heroWidget(title: "Flutter Map")),
-            FittedBox(
-              child: Text(
-                'Flutter Map',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40.0,
-                  letterSpacing: 5.0,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/EarthRotating.json', height: 200),
+                FittedBox(child: heroWidget(title: "Flutter Map")),
+                FittedBox(
+                  child: Text(
+                    'Flutter Map',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40.0,
+                      letterSpacing: 5.0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            FilledButton(
-              onPressed: () {
-                Navigator.push(
-                  //here instead of push we can also use pushreplacement which actually replaces the page instead of stacking it.
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      //here instead of push we can also use pushreplacement which actually replaces the page instead of stacking it.
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage(title: "Register",);
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Get Started'),
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 45.0),
+                    backgroundColor: const Color.fromARGB(255, 55, 136, 228),
                   ),
-                );
-              },
-              child: Text('Get Started'),
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 45.0),
-                backgroundColor: const Color.fromARGB(255, 55, 136, 228),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  //here instead of push we can also use pushreplacement which actually replaces the page instead of stacking it.
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      //here instead of push we can also use pushreplacement which actually replaces the page instead of stacking it.
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage(title: "Login",);
+                        },
+                      ),
+                    );
+                  },
+                  child: Text("Login"),
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 45.0),
                   ),
-                );
-              },
-              child: Text("Login In"),
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 45.0),
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
