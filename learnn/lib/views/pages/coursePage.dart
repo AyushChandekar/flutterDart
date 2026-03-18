22c3d9be7cb5450fa29c113dbb39e24a8d466045
AyushChandekar <ayush.pc19@gmail.com>
2026-03-18 21:28:08 +0530

    class created for the api rendering

--- learnn/lib/views/pages/coursePage.dart
+++ learnn/lib/views/pages/coursePage.dart
@@ -1,7 +1,10 @@
-import 'dart:convert' as convert;
-import 'dart:developer';
+// import 'dart:convert' as convert;
+// import 'dart:developer';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:learnn/data/classes/activityClass.dart';
 import 'package:learnn/views/pages/onBoarding.dart';
 import 'package:learnn/views/widgets/heroWidget.dart';
 
@@ -12,6 +15,8 @@ class CoursePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CoursePage> {
+  late Activity
+  activity; //late means latter we will set this value and null for now
   @override
   void initState() {
     getData();
@@ -22,17 +27,28 @@ class _CoursePageState extends State<CoursePage> {
     var url = Uri.https('bored-api.appbrewery.com', '/random');
 
     var response = await http.get(url);
+    // if (response.statusCode == 200) {
+    //   var jsonResponse =
+    //       convert.jsonDecode(response.body) as Map<String, dynamic>;
+    //   var itemCount = jsonResponse['activity'];
+    //   print(itemCount);
+    //   log(response.body);
+    // } else {
+    //   print('Request failed with status: ${response.statusCode}.');
+    // }
     if (response.statusCode == 200) {
-      var jsonResponse =
-          convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['activity'];
-      print(itemCount);
-      log(response.body);
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      activity = Activity.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
+      print(activity.activity);
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      throw Exception('Failed to load album');
     }
   }
 
+  // if the data is coming late from server through api and not present so,for this we'll use future builder
   @override
   Widget build(BuildContext context) {
     return Scaffold(
